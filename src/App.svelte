dc3be0a7031edf1a439bb01ec1f80f1137d19034
Mike Evans <mike.evans at numical.com>
2020-03-15 15:02:51 +0000

    add fixed height header

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,41 @@
 <script>
-	const gridSize = 11;
+	const numCols = 5;
+	const numRows = 5;
 	const colours = ['darkblue', 'blue', 'cornflowerblue', 'lightblue'];
-	const cells = Array(gridSize * gridSize).fill(0).map((_, index) => ({
+	const cells = Array(numCols * numCols).fill(0).map((_, index) => ({
 		text: `Cell ${index + 1}`,
 		colour: colours[(index % colours.length)]
 	}));
 	const gridInstructions = `
-		grid-template-columns: repeat(${gridSize}, ${100/gridSize}vw);
-		grid-template-rows: repeat(${gridSize}, ${100/gridSize}vh);
+		grid-template-columns: repeat(${numCols}, ${100/numCols}vw);
+		grid-template-rows: repeat(${numRows}, calc((100vh - 2rem - 10px)/${numRows}));
 	`;
 
 </script>
 
-<main class="gridContainer" style="{gridInstructions}">
-	{#each cells as cell}
-		<div style="background-color: {cell.colour}">{cell.text}</div>
-	{/each}
+<main>
+	<div class="title">
+		{`A ${numCols} x ${numRows} grid`}
+	</div>
+	<div class="gridContainer" style="{gridInstructions}">
+		{#each cells as cell}
+			<div class="gridCell" style="background-color: {cell.colour}">{cell.text}</div>
+		{/each}
+	</div>
 </main>
 
 <style>
+	.title {
+		font-size: 2rem;
+		text-align: center;
+		height: calc(2rem + 10px);
+	}
 	.gridContainer {
 		display: grid;
 		width: 100vw;
-		height: 100vh;
 	}
 
-	div {
+	.gridCell {
 		color: white;
 		display: flex;
 		align-items: center;
