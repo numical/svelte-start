e9eeccc27d745f0c8089be873b72938055d272bd
Mike Evans <mike.evans at numical.com>
2020-03-15 12:04:21 +0000

    dynamically generate cells

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,28 @@
 <script>
-	export let name;
+	const gridSize = 11;
+	const colours = ['darkblue', 'blue', 'cornflowerblue', 'lightblue'];
+	const cells = Array(gridSize * gridSize).fill(0).map((_, index) => ({
+		text: `Cell ${index + 1}`,
+		colour: colours[(index % colours.length)]
+	}));
+	const gridInstructions = `
+		grid-template-columns: repeat(${gridSize}, ${100/gridSize}vw);
+		grid-template-rows: repeat(${gridSize}, ${100/gridSize}vh);
+	`;
+
 </script>
 
-<main>
-	<div class="one">Div 1</div>
-	<div class="two">Div 2</div>
-	<div class="three">Div 3</div>
-	<div class="four">Div 4</div>
+<main class="gridContainer" style="{gridInstructions}">
+	{#each cells as cell}
+		<div style="background-color: {cell.colour}">{cell.text}</div>
+	{/each}
 </main>
 
 <style>
-	main {
+	.gridContainer {
 		display: grid;
 		width: 100vw;
 		height: 100vh;
-		grid-template-columns: repeat(2, 50vmax);
-		grid-template-rows: repeat(2, 50vmin);
 	}
 
 	div {
@@ -23,19 +30,6 @@
 		display: flex;
 		align-items: center;
 		justify-content: center;
-	}
-
-	.one {
-		background-color: darkblue;
-	}
-	.two {
-		background-color: blue;
-	}
-	.three {
-		background-color: cornflowerblue;
-	}
-	.four {
-		background-color: lightblue;
 	}
 
 	@media (min-width: 640px) {
